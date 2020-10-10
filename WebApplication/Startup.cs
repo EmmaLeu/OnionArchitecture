910db52fb2bb3b70fe51f47ea55cd8a958b4c146
EmmaUiPath <manuela.leu@uipath.com>
2020-10-10 19:16:19 +0300

    Add Application interfaces; Register Application layer services in WebApplication

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -1,5 +1,7 @@
+using Application;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -33,6 +35,8 @@ namespace WebApplication
                     Title = "OnionArchitecture"
                 });
             });
+
+            services.AddApplication();
         }
 
         ///<summary>This method gets called by the runtime. Use this method to configure the HTTP request pipeline.</summary>
